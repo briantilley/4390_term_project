0d4494bcddbef3ec40b75fe7ca03623ece26f0b0
DBell <derekbellanca@gmail.com>
2018-04-27 18:04:26 -0500

    debugged

--- controller.py
+++ controller.py
@@ -110,45 +110,44 @@ def controller():
      answer = input("Enter Yes(Y) or No (N)\n")
 
      temp=True
-
      while temp == True:
           if answer.lower()=="n" or answer.lower()=="no":
                break
           elif answer.lower()=="y" or answer.lower()=="yes":
-               index = get_index(server_socket)
-               print(*index, sep = "\n")
+               #index = get_index(server_socket)
+               index = [1, 2, 3, 4]
+               for x in range(len(index)):
+                    print(x)
                desiredFiles = raw_input("Which files would you like to view?\nEnter files as comma separated list").split(",")
 			   
                example_file_str = None
-                    for file in desiredFiles:
-                         try:
-                              with open(desiredFiles, "r") as f:
-                                   example_file_str = f.read()			  
-                                   play(example_file_str, display_socket)
-                         except IOError as err:
-                              print("Cannot open ", desiredFiles)
-                              break
-
-                         keep_going = True
-                         while keep_going:
-                              cmd = input("(p)ause, (r)esume, (s)top: ")[0].lower()
-                              if 'p' == cmd:
-                                   pause()
-                              elif 'r' == cmd:
-                                   resume()
-                              elif 's' == cmd:
-                                   stop()
-                                   keep_going = False
-                              else:
-                                   print("wot")
-
+               for file in desiredFiles:
+                    try:
+                         with open(desiredFiles, "r") as f:
+                              example_file_str = f.read()			  
+                              play(example_file_str, display_socket)
+                    except IOError as err:
+                         print("Cannot open ", desiredFiles)
                          break
+
+                    keep_going = True
+                    while keep_going:
+                         cmd = input("(p)ause, (r)esume, (s)top: ")[0].lower()
+                         if 'p' == cmd:
+                              pause()
+                         elif 'r' == cmd:
+                              resume()
+                         elif 's' == cmd:
+                              stop()
+                              keep_going = False
+                         else:
+                              print("wot")
+                    break
 
           else:
                print("\nWrong Input!\n")
                print("Would you like to list the text files?\n")
                answer = input("Enter Yes(Y) or No (N)")
-
 
 if __name__ == "__main__":
      controller()
